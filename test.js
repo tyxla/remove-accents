0a2af9a33be37a81e95fc3e7c057b94573903550
Peter Johnson <insomnia@rcpt.at>
2023-07-18 20:46:11 +0200

    refactor: experiment using unicode decomposition+recomposition and the regenerate lib for pattern matching

--- test.js
+++ test.js
@@ -1,50 +1,50 @@
-var tape = require('tape');
-var removeAccents = require('./');
+const tape = require('tape')
+let removeAccents = require('./')
 
-tape('remove accents from string', function(t) {
-	var input = 'ÀÁÂÃÄÅẤẮÆẦẰÇḈÈÉÊËẾḖỀḔÌÍÎÏḮÐÑÒÓÔÕÖØỐṌṒÙÚÛÜÝàáâãäåấắæầằçḉèéêëếḗềḕìíîïḯñòóôõöøốṍṓùúûüýÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģǴǵĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķḰḱĹĺĻļĽľĿŀŁłḾḿŃńŅņŇňŉŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵẂẃŶŷŸŹźŻżŽžſƒƠơƯưǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜỨứṸṹǺǻǼǽǾǿðÞþṔṕṤṥX́x́ЃѓЌќA̋a̋E̋e̋I̋i̋ǸǹỒồṐṑỪừẀẁỲỳȀȁȄȅȈȉȌȍȐȑȔȕẲẴẶḜẳẵặḝC̆c̆ḪḫK̆k̆M̆m̆N̆n̆P̆p̆R̆r̆T̆t̆V̆v̆X̆x̆Y̆y̆ȂȆȊȎȃȇȋȏȒȓȖȗșțȘȚB̌b̌F̌f̌ǦǧȞȟJ̌ǰǨǩM̌m̌P̌p̌Q̌q̌ṦṧV̌v̌W̌w̌X̌x̌Y̌y̌A̧a̧B̧b̧ḐḑȨȩƐ̧ɛ̧ḨḩI̧i̧Ɨ̧ɨ̧M̧m̧O̧o̧Q̧q̧U̧u̧X̧x̧Z̧z̧ß';
-	var output = removeAccents(input);
-        var expected = 'AAAAAAAAAEAACCEEEEEEEEIIIIIDNOOOOOOOOOUUUUYaaaaaaaaaeaacceeeeeeeeiiiiinooooooooouuuuyyAaAaAaCcCcCcCcDdDdEeEeEeEeEeGgGgGgGgGgHhHhIiIiIiIiIiIJijJjKkKkLlLlLlLlllMmNnNnNnnOoOoOoOEoeRrRrRrSsSsSsSsTtTtTtUuUuUuUuUuUuWwWwYyYZzZzZzsfOoUuAaIiOoUuUuUuUuUuUuUuAaAEaeOodTHthPpSsXxГгКкAaEeIiNnOoOoUuWwYyAaEeIiOoRrUuAAAEaaaeCcHhKkMmNnPpRrTtVvXxYyAEIOaeioRrUustSTBbFfGgHhJjKkMmPpQqSsVvWwXxYyAaBbDdEeEeHhIiIiMmOoQqUuXxZzss';
+tape('remove accents from string', function (t) {
+  const input = 'ÀÁÂÃÄÅẤẮÆẦẰÇḈÈÉÊËẾḖỀḔÌÍÎÏḮÐÑÒÓÔÕÖØỐṌṒÙÚÛÜÝàáâãäåấắæầằçḉèéêëếḗềḕìíîïḯñòóôõöøốṍṓùúûüýÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģǴǵĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķḰḱĹĺĻļĽľĿŀŁłḾḿŃńŅņŇňŉŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵẂẃŶŷŸŹźŻżŽžſƒƠơƯưǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜỨứṸṹǺǻǼǽǾǿðÞþṔṕṤṥX́x́ЃѓЌќA̋a̋E̋e̋I̋i̋ǸǹỒồṐṑỪừẀẁỲỳȀȁȄȅȈȉȌȍȐȑȔȕẲẴẶḜẳẵặḝC̆c̆ḪḫK̆k̆M̆m̆N̆n̆P̆p̆R̆r̆T̆t̆V̆v̆X̆x̆Y̆y̆ȂȆȊȎȃȇȋȏȒȓȖȗșțȘȚB̌b̌F̌f̌ǦǧȞȟJ̌ǰǨǩM̌m̌P̌p̌Q̌q̌ṦṧV̌v̌W̌w̌X̌x̌Y̌y̌A̧a̧B̧b̧ḐḑȨȩƐ̧ɛ̧ḨḩI̧i̧Ɨ̧ɨ̧M̧m̧O̧o̧Q̧q̧U̧u̧X̧x̧Z̧z̧ß'
+  let output = removeAccents(input)
+  let expected = 'AAAAAAAAAEAACCEEEEEEEEIIIIIDNOOOOOOOOOUUUUYaaaaaaaaaeaacceeeeeeeeiiiiinooooooooouuuuyyAaAaAaCcCcCcCcDdDdEeEeEeEeEeGgGgGgGgGgHhHhIiIiIiIiIiIJijJjKkKkLlLlLlLlllMmNnNnNnnOoOoOoOEoeRrRrRrSsSsSsSsTtTtTtUuUuUuUuUuUuWwWwYyYZzZzZzsfOoUuAaIiOoUuUuUuUuUuUuUuAaAEaeOodTHthPpSsXxГгКкAaEeIiNnOoOoUuWwYyAaEeIiOoRrUuAAAEaaaeCcHhKkMmNnPpRrTtVvXxYyAEIOaeioRrUustSTBbFfGgHhJjKkMmPpQqSsVvWwXxYyAaBbDdEeEeHhIiIiMmOoQqUuXxZzss'
 
-	t.same( output, expected );
+  t.same(output, expected)
 
-	t.end();
-});
+  t.end()
+})
 
-tape('remove cyrillic accents from string', function(t) {
-	var input = 'ЁёЙй';
-	var output = removeAccents(input);
-	var expected = 'ЕеИи';
+tape('remove cyrillic accents from string', function (t) {
+  const input = 'ЁёЙй'
+  let output = removeAccents(input)
+  let expected = 'ЕеИи'
 
-	t.same( output, expected );
+  t.same(output, expected)
 
-	t.end();
-});
+  t.end()
+})
 
-tape('do not modify non-accented strings', function(t) {
-	var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789.,:;~`!@#$%^&*()-_=+[]{}\'"|\\<>?/eEиИ';
-	var output = removeAccents(input);
+tape('do not modify non-accented strings', function (t) {
+  const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789.,:;~`!@#$%^&*()-_=+[]{}\'"|\\<>?/eEиИ'
+  let output = removeAccents(input)
 
-	t.same( output, input );
+  t.same(output, input)
 
-	t.end();
-});
+  t.end()
+})
 
-tape('.has can detect accents', function(t) {
-	t.equal(removeAccents.has('À'), true);
-	t.equal(removeAccents.has('Löwe'), true);
+tape('.has can detect accents', function (t) {
+  t.equal(removeAccents.has('À'), true)
+  t.equal(removeAccents.has('Löwe'), true)
 
-	t.equal(removeAccents.has('A'), false);
-	t.equal(removeAccents.has('Panther'), false);
+  t.equal(removeAccents.has('A'), false)
+  t.equal(removeAccents.has('Panther'), false)
 
-	t.end();
-});
+  t.end()
+})
 
-tape('.remove method', function(t) {
-	t.same(removeAccents.toString(), removeAccents.remove.toString());
+tape('.remove method', function (t) {
+  t.same(removeAccents.toString(), removeAccents.remove.toString())
 
-	t.same(removeAccents.remove('cat'), 'cat');
-	t.same(removeAccents.remove('Pokémon'), 'Pokemon');
-	t.same(removeAccents.remove('Straße'), 'Strasse');
-	t.end();
-});
+  t.same(removeAccents.remove('cat'), 'cat')
+  t.same(removeAccents.remove('Pokémon'), 'Pokemon')
+  t.same(removeAccents.remove('Straße'), 'Straße')
+  t.end()
+})
